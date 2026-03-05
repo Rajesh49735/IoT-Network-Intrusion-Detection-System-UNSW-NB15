b4503540e641eeea9aaeb7cd2e78bfc14f0e9b07
PONRAJESH M <119751003+Rajesh49735@users.noreply.github.com>
2026-03-05 15:28:09 +0530

    Update response messages in receiver.py

--- receiver.py
+++ receiver.py
@@ -2,17 +2,17 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-# Test page
+# Homepage route
 @app.route('/')
 def home():
-    return "IoT IDS Receiver Running"
+    return "IoT Intrusion Detection Server Running"
 
 # ESP8266 will send data here
-@app.route('/data', methods=['POST','GET'])
+@app.route('/data', methods=['POST', 'GET'])
 def receive():
     data = request.data.decode()
     print("Traffic received:", data)
-    return "OK"
+    return "Data received successfully"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
